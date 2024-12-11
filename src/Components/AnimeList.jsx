8e5f54c13168eb08ec08b077ab80236db4cab12c
usagi. <solevoi.ghoul@gmail.com>
2024-12-11 22:46:54 +0300

    add main poster to home page

--- src/Components/AnimeList.jsx
+++ src/Components/AnimeList.jsx
@@ -1,26 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import "../Styles/AnimeList.css";
-import initSqlJs from 'sql.js';
-import AnimeCard from './AnimeCard.jsx';
+import initSqlJs from "sql.js";
+import AnimeCard from "./AnimeCard.jsx";
 
 export default function AnimeList() {
-    const [animeList, setAnimeList] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [animeList, setAnimeList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const loadDatabase = async () => {
-            try {
-                const SQL = await initSqlJs({ locateFile: file => `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.12.0/${file}` });
+  useEffect(() => {
+    const loadDatabase = async () => {
+      try {
+        const SQL = await initSqlJs({
+          locateFile: (file) =>
+            `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.12.0/${file}`,
+        });
 
-                const response = await fetch('/anime.db');
-                if (!response.ok) {
-                    throw new Error('Не удалось загрузить базу данных.');
-                }
+        const response = await fetch("/anime.db");
+        if (!response.ok) {
+          throw new Error("Не удалось загрузить базу данных.");
+        }
 
-                const buffer = await response.arrayBuffer();
-                const db = new SQL.Database(new Uint8Array(buffer));
+        const buffer = await response.arrayBuffer();
+        const db = new SQL.Database(new Uint8Array(buffer));
 
-                const query = `
+        const query = `
                     SELECT a.id, a.title, a.description, a.release_year, a.rating, a.image_url, 
                         GROUP_CONCAT(g.name, ', ') AS genres
                     FROM Anime a
@@ -29,46 +32,46 @@ export default function AnimeList() {
                     GROUP BY a.id
                     LIMIT 10;
                 `;
-                const result = db.exec(query);
+        const result = db.exec(query);
 
-                if (result.length > 0) {
-                    const columns = result[0].columns;
-                    const values = result[0].values;
-                    const animeData = values.map(row => {
-                        const anime = {};
-                        columns.forEach((col, index) => {
-                            anime[col] = row[index];
-                        });
-                        return anime;
-                    });
-                    setAnimeList(animeData);
-                } else {
-                    console.warn('Нет данных в таблице Anime.');
-                }
+        if (result.length > 0) {
+          const columns = result[0].columns;
+          const values = result[0].values;
+          const animeData = values.map((row) => {
+            const anime = {};
+            columns.forEach((col, index) => {
+              anime[col] = row[index];
+            });
+            return anime;
+          });
+          setAnimeList(animeData);
+        } else {
+          console.warn("Нет данных в таблице Anime.");
+        }
 
-                db.close();
-            } catch (error) {
-                console.error('Ошибка при работе с базой данных:', error);
-            } finally {
-                setLoading(false);
-            }
-        };
+        db.close();
+      } catch (error) {
+        console.error("Ошибка при работе с базой данных:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        loadDatabase();
-    }, []);
+    loadDatabase();
+  }, []);
 
-    if (loading) {
-        return <div>Загрузка...</div>;
-    }
+  if (loading) {
+    return <div>Загрузка...</div>;
+  }
 
-    return (
-        <div className="anime-catalog">
-            <h1>Каталог Аниме</h1>
-            <div className="anime-cards">
-                {animeList.map(anime => (
-                    <AnimeCard key={anime.image_url} anime={anime} />
-                ))}
-            </div>
-        </div>
-    );
-};
+  return (
+    <div className="anime-catalog">
+      <h1 className="anime-list-title">Каталог Аниме</h1>
+      <div className="anime-cards">
+        {animeList.map((anime) => (
+          <AnimeCard key={anime.image_url} anime={anime} />
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
-import AnimeList from '../Components/AnimeList.jsx';
+import React from "react";
+import AnimeList from "../Components/AnimeList.jsx";
+import "../Styles/HomePage.css";
 
 export default function App() {
-    return (
-        <AnimeList>
-        </AnimeList>
-    );
+  return (
+    <>
+      <div className="main-anime-container">
+        <div className="image-container">
+          <img
+            src="/main-poster.jpg"
+            alt="main-poster"
+            className="main-anime blurred"
+          />
+          <img
+            src="/main-poster.jpg"
+            alt="main-poster"
+            className="main-anime"
+          />
+        </div>
+
+        <div className="content-container">
+          <img
+            src="/main-anime-logo.png"
+            alt="main-anime-logo"
+            className="main-anime-logo"
+          />
+        </div>
+      </div>
+
+      <AnimeList />
+    </>
+  );
 }
