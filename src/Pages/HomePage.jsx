8ae97fdbc4cd779b15c0d01c2879673cd2adae3b
usagi. <solevoi.ghoul@gmail.com>
2024-12-12 10:04:13 +0300

    fix tabs

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,34 +1,34 @@
-import React from "react";
-import AnimeList from "../Components/AnimeList.jsx";
-import "../Styles/HomePage.css";
+import React from 'react'
+import AnimeList from '../Components/AnimeList.jsx'
+import '../Styles/HomePage.css'
 
 export default function App() {
-  return (
-    <>
-      <div className="main-anime-container">
-        <div className="image-container">
-          <img
-            src="/main-poster.jpg"
-            alt="main-poster"
-            className="main-anime blurred"
-          />
-          <img
-            src="/main-poster.jpg"
-            alt="main-poster"
-            className="main-anime"
-          />
-        </div>
+    return (
+        <>
+            <div className="main-anime-container">
+                <div className="image-container">
+                    <img
+                        src="/main-poster.jpg"
+                        alt="main-poster"
+                        className="main-anime blurred"
+                    />
+                    <img
+                        src="/main-poster.jpg"
+                        alt="main-poster"
+                        className="main-anime"
+                    />
+                </div>
 
-        <div className="content-container">
-          <img
-            src="/main-anime-logo.png"
-            alt="main-anime-logo"
-            className="main-anime-logo"
-          />
-        </div>
-      </div>
+                <div className="content-container">
+                    <img
+                        src="/main-anime-logo.png"
+                        alt="main-anime-logo"
+                        className="main-anime-logo"
+                    />
+                </div>
+            </div>
 
-      <AnimeList />
-    </>
-  );
+            <AnimeList />
+        </>
+    )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './Styles/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>
 )
