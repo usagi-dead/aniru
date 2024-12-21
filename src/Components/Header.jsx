e071becf2e9e4cc4e8371c1f776e4db97000a973
usagi. <solevoi.ghoul@gmail.com>
2024-12-21 16:08:14 +0300

    fix header scrolled

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { AuthContext } from '../Context/AuthContext'
 import '../Styles/Header.css'
@@ -7,14 +7,31 @@ import Search from './Search'
 const Header = () => {
     const { user, logout } = useContext(AuthContext)
     const [animation, setAnimation] = useState(false)
+    const [scrolled, setScrolled] = useState(false)
 
     setTimeout(() => {
         setAnimation(true)
     }, 600)
 
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setScrolled(true)
+            } else {
+                setScrolled(false)
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
     return (
-        <header className={animation ? 'active' : ''}>
-            <div className="header-back">
+        <header className={`${animation ? 'active' : ''}`}>
+            <div className={`header-back ${scrolled ? 'scrolled' : ''}`}>
                 <div className="header-align container">
                     <Link to="/">
                         <img src="/aniru.svg" alt="aniru" className="logo" />
