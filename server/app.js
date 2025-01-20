f433ca0c625d43722a58277f52319b9667be9a60
usagi. <solevoi.ghoul@gmail.com>
2025-01-20 14:56:03 +0300

    fix cors

--- server/app.js
+++ server/app.js
@@ -32,6 +32,8 @@ app.use(
             }
         },
         credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
     })
 )
 
