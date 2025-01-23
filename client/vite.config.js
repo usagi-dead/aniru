2d3cbde44541a5f02f65790aecc4d08ffd0916be
usagi. <solevoi.ghoul@gmail.com>
2025-01-23 23:30:51 +0300

    add https redirects

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: '_redirects',
+                    dest: '.',
+                },
+            ],
+        }),
+    ],
     server: {
         proxy: {
             '/api': {
